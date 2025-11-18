5d37bee5ea9b44bfe4d965348dbab5cc719ff0f3
VSU-Z <IzeRibak1@mail.ru>
2025-11-18 12:14:19 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -8,20 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
     var name = document.getElementById("name").value;
     var mail = document.getElementById("mail").value;
     var mess = document.getElementById("mess").value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    block.setAttribute("class", "ans");
-    bacund.setAttribute("class", "bg")
-    block.innerHTML = `
-    <p>ДАННЫЕ</p>
-    <p>Имя: <span>${name}</span></p>
+    if (name != "" && emailRegex.test(mail) ) {
+      block.setAttribute("class", "ans");
+      bacund.setAttribute("class", "bg")
+      block.innerHTML = `
+      <p>ДАННЫЕ</p>
+      <p>Имя: <span>${name}</span></p>
 
-    <p>Почта: <span>${mail}</span></p>
+      <p>Почта: <span>${mail}</span></p>
 
-    <p>Сообщение: <span>${mess}</span></p>
+      <p>Сообщение: <span>${mess}</span></p>
 
-    `;
-    document.body.appendChild(block);
-    document.body.appendChild(bacund);
+      `;
+      document.body.appendChild(block);
+      document.body.appendChild(bacund);
+    }
+    else if (emailRegex.test(mail) == false) {
+      alert("Неверно введена почта");
+    }
+    else if (name === "") {
+      alert("Имя не введено");
+    }
   });
   bacund.addEventListener("click", () => {
     document.body.removeChild(bacund);
